b2b3483f77238d67aa296276f7094f1f56049b52
yanyv-915 <3467286803@qq.com>
2025-08-03 22:29:44 +0800

    客户端端口创建

--- Day2_net/client.cpp
+++ Day2_net/client.cpp
@@ -4,15 +4,18 @@
 #include<cstring>
 using namespace std;
 int main(){
+    //创建 socket         → socket()
     int client_fd=socket(AF_INET,SOCK_STREAM,0);
 
+    //设置服务器地址结构   → sockaddr_in + inet_pton()
     sockaddr_in client_addr{};
     client_addr.sin_family=AF_INET;
     client_addr.sin_port=htons(12345);
     inet_pton(AF_INET, "127.0.0.1", &client_addr.sin_addr);
-
+    //发起连接请求        → connect()
     connect(client_fd,(sockaddr*)&client_addr,sizeof(client_addr));
 
+    //通信 + 关闭连接     → write()/read()/close()
     const char* msg="Hello server!";
     write(client_fd,msg,strlen(msg));
 
