f5b952f8d1ba18c80b64b738bb149c4106b8af76
yanyv-915 <3467286803@qq.com>
2025-08-06 22:27:42 +0800

    服务端生成并打印服务端IP地址

--- Day3_net/client.cpp
+++ Day3_net/client.cpp
@@ -1,25 +1,26 @@
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<iostream>
-#include<cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
 using namespace std;
-int main(){
-    //生成客户端端口
-    int sock_fd=socket(AF_INET,SOCK_STREAM,0);
 
-    //设定客户端端口地址
+int main() {
+    // 创建套接字（AF_INET 表示 IPv4，SOCK_STREAM 表示 TCP）
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // 配置服务器地址结构（使用 IPv4，端口 12345）
     sockaddr_in c_addr{};
-    c_addr.sin_family=AF_INET;
-    c_addr.sin_port=htons(12345);
+    c_addr.sin_family = AF_INET;
+    c_addr.sin_port = htons(12345);  // 主机字节序转网络字节序
 
-    //将字符串形式的IP地址转化为二进制格式的可绑定地址格式
-    inet_pton(AF_INET,"127.0.0.1",&c_addr.sin_addr.s_addr);
+    // 将字符串形式的 IP 地址转换为网络字节序的二进制形式
+    inet_pton(AF_INET, "127.0.0.1", &c_addr.sin_addr.s_addr);
 
-    //连接客户端端口文件
-    connect(sock_fd,(sockaddr*)&c_addr,sizeof(c_addr));
-    cout<<"连接成功！"<<endl;
+    // 发起连接请求，连接目标服务器
+    connect(sock_fd, (sockaddr*)&c_addr, sizeof(c_addr));
+    cout << "连接成功！" << endl;
 
-    //关闭端口
+    // 关闭套接字，断开连接
     close(sock_fd);
     return 0;
 }
