1b24aac64eff0a6bc5de8396a25d4ff2d87e6de7
yanyv-915 <3467286803@qq.com>
2025-08-06 21:17:47 +0800

    可执行文件

--- Day3_net/client.cpp
+++ Day3_net/client.cpp
@@ -10,9 +10,7 @@ int main(){
     c_addr.sin_family=AF_INET;
     c_addr.sin_port=htons(12345);
     inet_pton(AF_INET,"127.0.0.1",&c_addr.sin_addr.s_addr);
-
     connect(sock_fd,(sockaddr*)&c_addr,sizeof(c_addr));
-
     cout<<"连接成功！"<<endl;
     close(sock_fd);
     return 0;
